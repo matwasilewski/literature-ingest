6ae226b6d647ac2866369f5324883de6c418b777
matwasilewski <mat.p.wasilewski@gmail.com>
2025-01-17 13:51:07 +0000

    add unzip

--- src/literature_ingest/data_engineering.py
+++ src/literature_ingest/data_engineering.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import tarfile
+import tempfile
 from typing import List
+
+from cloudpathlib import CloudPath
 
 def resolve_file_or_dir(target: Path, source: Path) -> Path:
     if target.is_dir():
@@ -9,17 +12,36 @@ def resolve_file_or_dir(target: Path, source: Path) -> Path:
         return target
 
 def unzip_and_filter(archive_file: Path, target_dir: Path, extension = ".xml") -> List[Path]:
+
     files = []
 
-    with tarfile.open(archive_file, "r:gz") as tar:
-        for member in tar.getmembers():
-            if member.isfile() and member.name.endswith(extension):
-                # Get just the filename without the path
-                filename = Path(member.name).name
-                # Create new destination path in target directory
-                file_path = target_dir / filename
-                # Extract member to the target directory, renaming it
-                member.name = filename
-                tar.extract(member, target_dir)
-                files.append(file_path)
+    if isinstance(target_dir, CloudPath):
+        target_dir = Path(target_dir)
+        # unzip locally and upload to target_dir
+        with tempfile.TemporaryDirectory() as local_dir:
+            with tarfile.open(archive_file, "r:gz") as tar:
+                for member in tar.getmembers():
+                    if member.isfile() and member.name.endswith(extension):
+                        # Get just the filename without the path
+                        filename = Path(member.name).name
+                        # Create new destination path in target directory
+                        local_file_path = local_dir / filename
+                        target_file_path = target_dir / filename
+                        # Extract member to the target directory, renaming it
+                        member.name = filename
+                        tar.extract(member, target_dir)
+                        files.append(target_file_path)
+                        local_file_path.copy(target_file_path)
+    else:
+        with tarfile.open(archive_file, "r:gz") as tar:
+            for member in tar.getmembers():
+                if member.isfile() and member.name.endswith(extension):
+                    # Get just the filename without the path
+                    filename = Path(member.name).name
+                    # Create new destination path in target directory
+                    target_file_path = target_dir / filename
+                    # Extract member to the target directory, renaming it
+                    member.name = filename
+                    tar.extract(member, target_dir)
+                    files.append(target_file_path)
     return files
