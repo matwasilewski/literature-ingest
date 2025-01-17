28cb86ff8da5b5d219c56b90e010497f0e1d79a9
matwasilewski <mat.p.wasilewski@gmail.com>
2025-01-17 11:19:06 +0000

    fix config

--- src/literature_ingest/utils/config.py
+++ src/literature_ingest/utils/config.py
@@ -17,7 +17,7 @@ def _get_project_meta(name: str = "unknown") -> Dict:
     try:
         with Path("./pyproject.toml").open() as pyproject:
             file_contents = pyproject.read()
-        parsed = dict(tomlkit.parse(file_contents))["tool"]["poetry"]
+        parsed = dict(tomlkit.parse(file_contents))['project']
         name = parsed["name"]
         version = parsed.get("version", "unknown")
         description = parsed.get("description", "")
