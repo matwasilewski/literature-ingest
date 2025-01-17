6cb44a5b864eb3014ee8b0bf1a98fd18ba65bc2d
matwasilewski <mat.p.wasilewski@gmail.com>
2025-01-17 13:30:19 +0000

    add basic Data Engineering scripts

--- src/literature_ingest/data_engineering.py
+++ src/literature_ingest/data_engineering.py
@@ -1,5 +1,8 @@
 from pathlib import Path
-
+import tarfile
+from typing import List
+import zipfile
+import requests
 
 def resolve_file_or_dir(target: Path, source: Path) -> Path:
     if target.is_dir():
@@ -7,5 +10,13 @@ def resolve_file_or_dir(target: Path, source: Path) -> Path:
     else:
         return target
 
-def unzip(file: Path, target: Path) -> None:
-    pass
+def unzip_and_filter(archive_file: Path, target_dir: Path, extension = ".xml") -> List[Path]:
+    files = []
+
+    with tarfile.open(archive_file, "r:gz") as tar:
+        for member in tar.getmembers():
+            if member.isfile() and member.name.endswith(extension):
+                file_path = target_dir / member.name
+                tar.extract(member, file_path)
+                files.append(file_path)
+    return files
