07c744188e2947bf6b10790e55fbc9102edda80a
matwasilewski <mat.p.wasilewski@gmail.com>
2025-01-17 14:31:22 +0000

    add pmc doc fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,9 @@ import pytest
 @pytest.fixture(scope="session")
 def test_resources_root() -> Path:
     return Path(__file__).parent / "resources"
+
+
+@pytest.fixture
+def pmc_doc(test_resources_root: Path) -> str:
+    with open(test_resources_root / "PMC10335194.xml", "r") as f:
+        return f.read()
