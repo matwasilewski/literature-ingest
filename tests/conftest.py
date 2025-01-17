ccb42b14beffe43ec7b96f2f6868a3bb50d47b9c
matwasilewski <mat.p.wasilewski@gmail.com>
2025-01-17 16:05:26 +0000

    add new test file

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,3 +13,9 @@ def test_resources_root() -> Path:
 def pmc_doc(test_resources_root: Path) -> str:
     with open(test_resources_root / "PMC10335194.xml", "r") as f:
         return f.read()
+
+
+@pytest.fixture
+def pmc_doc_2(test_resources_root: Path) -> str:
+    with open(test_resources_root / "PMC10335194.xml", "r") as f:
+        return f.read()
