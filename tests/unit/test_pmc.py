c7e543284791aa7c766bc64ea5f7b082be842b0c
matwasilewski <mat.p.wasilewski@gmail.com>
2025-01-17 11:48:46 +0000

    add test to PMC

---
+++ tests/unit/test_pmc.py
@@ -0,0 +1,28 @@
+import pytest
+from unittest.mock import Mock
+from literature_ingest.pmc import PMCFTPClient
+
+def test_get_baseline_date_success():
+    # Arrange
+    client = PMCFTPClient()
+
+    # Act
+    result_dates, result_files = client.extract_baseline_files([
+        "oa_noncomm_xml.PMC000xxxxxx.baseline.2024-12-18.filelist.csv",
+        "some_other_file.txt"
+    ])
+
+    # Assert
+    assert result_dates == ["2024-12-18"]
+    assert result_files == ["oa_noncomm_xml.PMC000xxxxxx.baseline.2024-12-18.filelist.csv"]
+
+@pytest.mark.parametrize("file_list", [
+    ["oa_noncomm_xml.PMC000xxxxxx.baseline.invalid-date.filelist.csv"]  # invalid date format
+])
+def test_get_baseline_date_failure(file_list):
+    # Arrange
+    client = PMCFTPClient()
+
+    # Act/Assert
+    with pytest.raises(ValueError, match="Found a file with `baseline` string but no date"):
+        client.extract_baseline_files(file_list)
