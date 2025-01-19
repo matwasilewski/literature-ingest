30eb33332904c1c79c2fadf7b7155cb83fdc32e5
Mateusz Wasilewski <mateusz.wasilewski@biorelate.com>
2025-01-19 14:47:08 +0000

    refactor pipelines

--- src/literature_ingest/pipelines.py
+++ src/literature_ingest/pipelines.py
@@ -18,22 +18,9 @@ from literature_ingest.pmc import PMCParser
 #                 clinical_trials.append(doc)
 #     return clinical_trials
 
-def parse_pmc_docs_sample(start_from_parsed: bool = False):
-    source_dir = Path("data/pipelines/raw/")
-    unzipped_dir = Path("data/pipelines/unzipped/")
-    parsed_dir = Path("data/pipelines/parsed/")
-
-    parsed_dir.mkdir(parents=True, exist_ok=True)
-
+def pipeline_parse_pmc(unzipped_dir: Path, parsed_dir: Path = Path("data/pipelines/pmc/parsed/")):
     parser = PMCParser()
-
-    if not start_from_parsed:
-        print(f"Unzipping {source_dir}...")
-        for file in source_dir.glob("*.tar.gz"):
-            print(f"Unzipping {file}...")
-            unzipped_files_list = unzip_and_filter(file, unzipped_dir, extension=".xml", use_gsutil=False, overwrite=True)
-            print(f"Unzipped {len(unzipped_files_list)} files...")
-    print(f"Unzipped {unzipped_dir}, to the total of {len(list(unzipped_dir.glob('*.xml')))} files...")
+    parsed_dir.mkdir(parents=True, exist_ok=True)
 
     print(f"Parsing {unzipped_dir}, totalling {len(list(unzipped_dir.glob('*.xml')))} files...")
     unzipped_files = [Path(x) for x in list(unzipped_dir.glob("*.xml"))]
@@ -42,5 +29,34 @@ def parse_pmc_docs_sample(start_from_parsed: bool = False):
     print(f"Parsed {len(parsed_files)} files...")
     print(f"Unique article types: {parser.unique_article_types}")
 
-if __name__ == "__main__":
-    parse_pmc_docs_sample()
+
+
+def pipeline_ingest_pmc():
+    # Create directories
+    source_dir = Path("data/pipelines/pmc/raw/")
+    unzipped_dir = Path("data/pipelines/pmc/unzipped/")
+    parsed_dir = Path("data/pipelines/pmc/parsed/")
+
+    source_dir.mkdir(parents=True, exist_ok=True)
+    unzipped_dir.mkdir(parents=True, exist_ok=True)
+    parsed_dir.mkdir(parents=True, exist_ok=True)
+
+    # Download data
+    pmc_client = PMCParser()
+
+    print("Downloading PMC baselines...")
+    pmc_client._download_pmc_baselines()
+    print("Downloading PMC incremental...")
+    pmc_client._download_pmc_incremental()
+    print(f"Downloaded {len(list(source_dir.glob('*.tar.gz')))} files...")
+    print("DONE: Download PMC data")
+
+    # Unzip data
+    print(f"Unzipping {source_dir}...")
+    for file in source_dir.glob("*.tar.gz"):
+        print(f"Unzipping {file}...")
+        unzipped_files_list = unzip_and_filter(file, unzipped_dir, extension=".xml", use_gsutil=False, overwrite=True)
+        print(f"Unzipped {len(unzipped_files_list)} files...")
+    print(f"Unzipped {unzipped_dir}, to the total of {len(list(unzipped_dir.glob('*.xml')))} files...")
+
+    pipeline_parse_pmc(unzipped_dir, parsed_dir)
