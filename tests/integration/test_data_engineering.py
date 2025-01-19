ee42720e02b23179c39e9fb9d25da465dbbaabd6
Mateusz Wasilewski <mateusz.wasilewski@biorelate.com>
2025-01-19 14:28:58 +0000

    fix test to not use IT bucket

--- tests/integration/test_data_engineering.py
+++ tests/integration/test_data_engineering.py
@@ -10,4 +10,7 @@ def test_unzip_and_filter(test_resources_root: Path) -> None:
     file_name = "oa_noncomm_xml.incr.2024-12-22.tar.gz"
     archive_file = test_resources_root / file_name
 
+    if not settings.TEST_BUCKET:
+        raise ValueError("TEST_BUCKET is not set - skipping test")
+
     unzip_and_filter(archive_file, CloudPath(f"gs://{settings.TEST_BUCKET}"), use_gsutil=True, overwrite=True)
