d5d278a9ed8f2aba5d17db46e772b90fc6078d8e
Mateusz Wasilewski <mateusz.wasilewski@biorelate.com>
2025-01-19 15:02:23 +0000

    extend

--- src/literature_ingest/pipelines.py
+++ src/literature_ingest/pipelines.py
@@ -19,6 +19,33 @@ def pipeline_parse_pmc(unzipped_dir: Path, parsed_dir: Path = Path("data/pipelin
     print(f"Unique article types: {parser.unique_article_types}")
 
 
+def pipeline_ingest_pmc_sample(source_dir: Path = Path("data/pipelines/sample_pmc/raw/"), unzipped_dir: Path = Path("data/pipelines/sample_pmc/unzipped/"), parsed_dir: Path = Path("data/pipelines/sample_pmc/parsed/")):
+        # Create directories
+    source_dir.mkdir(parents=True, exist_ok=True)
+    unzipped_dir.mkdir(parents=True, exist_ok=True)
+    parsed_dir.mkdir(parents=True, exist_ok=True)
+
+    # Download data
+    pmc_downloader = PMCFTPClient()
+
+    print("Downloading PMC baselines...")
+    pmc_downloader._download_pmc_baselines_sample(source_dir)
+    print(f"Downloaded {len(list(source_dir.glob('*.tar.gz')))} files...")
+    print("DONE: Download PMC data")
+
+    # Unzip data
+    print(f"Unzipping {source_dir}...")
+    for file in source_dir.glob("*.tar.gz"):
+        print(f"Unzipping {file}...")
+        unzipped_files_list = unzip_and_filter(file, unzipped_dir, extension=".xml", use_gsutil=False, overwrite=True)
+        print(f"Unzipped {len(unzipped_files_list)} files...")
+    print(f"Unzipped {unzipped_dir}, to the total of {len(list(unzipped_dir.glob('*.xml')))} files...")
+
+    print("Parsing PMC data..." )
+    pipeline_parse_pmc(unzipped_dir, parsed_dir)
+    print("DONE: Parse PMC data")
+
+
 
 def pipeline_ingest_pmc(source_dir: Path = Path("data/pipelines/pmc/raw/"), unzipped_dir: Path = Path("data/pipelines/pmc/unzipped/"), parsed_dir: Path = Path("data/pipelines/pmc/parsed/")):
     # Create directories
@@ -43,5 +70,8 @@ def pipeline_ingest_pmc(source_dir: Path = Path("data/pipelines/pmc/raw/"), unzi
         unzipped_files_list = unzip_and_filter(file, unzipped_dir, extension=".xml", use_gsutil=False, overwrite=True)
         print(f"Unzipped {len(unzipped_files_list)} files...")
     print(f"Unzipped {unzipped_dir}, to the total of {len(list(unzipped_dir.glob('*.xml')))} files...")
+    print("DONE: Unzip PMC data")
 
+    print("Parsing PMC data..." )
     pipeline_parse_pmc(unzipped_dir, parsed_dir)
+    print("DONE: Parse PMC data")
