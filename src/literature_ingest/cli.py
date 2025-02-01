c394da0a75fdc0fd2af8ba4d1038cd3fc6c1a9e4
matwasilewski <mat.p.wasilewski@gmail.com>
2025-02-01 00:32:08 +0000

    finish refactor of PMC parse

--- src/literature_ingest/cli.py
+++ src/literature_ingest/cli.py
@@ -4,7 +4,7 @@ from typing import List, Optional, Union
 import click
 from literature_ingest.data_engineering import unzip_and_filter
 from literature_ingest.normalization import normalize_document
-from literature_ingest.pipelines import pipeline_ingest_pmc, pipeline_ingest_pmc_sample, pipeline_ingest_pubmed, pipeline_ingest_pubmed_sample, pipeline_parse_missing_files_in_pmc, pipeline_parse_pubmed
+from literature_ingest.pipelines import pipeline_ingest_pmc, pipeline_ingest_pmc_sample, pipeline_ingest_pubmed, pipeline_parse_missing_files_in_pmc, pipeline_parse_pubmed
 from literature_ingest.pmc import PMC_OPEN_ACCESS_NONCOMMERCIAL_XML_DIR, PUBMED_OPEN_ACCESS_DIR, PMCFTPClient, PMCParser, PubMedFTPClient
 from literature_ingest.utils.logging import get_logger
 
@@ -239,9 +239,9 @@ def ingest_pubmed_sample(file_names: List[str]):
     click.echo("Ingesting Pubmed sample data...")
 
     # Define directories
-    raw_dir=Path("data/pipelines/sample_pubmed/raw/"),
-    unzipped_dir=Path("data/pipelines/sample_pubmed/unzipped/"),
-    parsed_dir=Path("data/pipelines/sample_pubmed/parsed/"),
+    raw_dir=Path("data/pipelines/sample_pubmed/raw/")
+    unzipped_dir=Path("data/pipelines/sample_pubmed/unzipped/")
+    parsed_dir=Path("data/pipelines/sample_pubmed/parsed/")
 
     # Create directories
     raw_dir.mkdir(parents=True, exist_ok=True)
@@ -262,16 +262,13 @@ def ingest_pubmed_sample(file_names: List[str]):
         unzipped_files_list = unzip_and_filter(file, unzipped_dir, extension=".xml", use_gsutil=False, overwrite=True)
         print(f"Unzipped {len(unzipped_files_list)} files...")
     print(f"Unzipped {unzipped_dir}, to the total of {len(list(unzipped_dir.glob('*.xml')))} files...")
-
     unzipped_files_list = list(unzipped_dir.glob("*.xml"))
-    print("Parsing PMC data..." )
+
+    # Parse data
+    print("Parsing PubMed data..." )
     parsed_files = pipeline_parse_pubmed(unzipped_files_list, parsed_dir)
     print(f"Parsed {len(parsed_files)} files...")
-    print("DONE: Parse PMC data")
-
-    pipeline_ingest_pubmed_sample(
-        file_names=file_names
-    )
+    print("DONE: Parse PubMed data")
     click.echo("DONE: Ingest Pubmed sample data")
 
 
